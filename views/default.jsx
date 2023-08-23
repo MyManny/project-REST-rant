716166143b7caf67f8a0339b884774164c9f69d5
MyManny <gmail.com>
2023-08-22 21:14:17 -0700

    um

--- views/default.jsx
+++ views/default.jsx
@@ -1,17 +1,19 @@
 const React = require('react')
 
-function Default (html) {
+function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
 
-module.exports = Default
+module.exports = Def
+
