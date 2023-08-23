5b2762ca28249e38b61893c6ba3fbfe8c8192918
MyManny <gmail.com>
2023-08-22 19:20:52 -0700

    finished

--- views/default.jsx
+++ views/default.jsx
@@ -1,6 +1,6 @@
 const React = require('react')
 
-function Def (html) {
+function Default (html) {
     return (
         <html>
             <head>
@@ -14,4 +14,4 @@ function Def (html) {
   }
   
 
-module.exports = Def
+module.exports = Default
