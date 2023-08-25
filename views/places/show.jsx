d4b4bb41dc49a8fa915b100638789476e3a1e75a
MyManny <gmail.com>
2023-08-24 21:36:14 -0700

    part 5 step 6

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,17 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+  Edit
+</a>     
+
+<form method="POST" action=""> 
+  <button type="submit" className="btn btn-danger">
+    Delete
+  </button>
+</form>     
+
+
           </main>
         </Def>
     )
