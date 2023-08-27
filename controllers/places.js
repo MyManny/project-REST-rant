27a386fea3d225610bba5bee5abd549907b418d6
MyManny <gmail.com>
2023-08-27 10:47:49 -0700

    set up edit form

--- controllers/places.js
+++ controllers/places.js
@@ -6,16 +6,24 @@ const places = require('../models/places.js')
 //     res.render('GET /places')
 // })
 // GET /places
-router.get('/', (req, res) => {
 
-      
-      res.render('places/index', { places })
-
-  })
   
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -29,7 +37,12 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id })
   }
 })
+router.get('/', (req, res) => {
 
+      
+  res.render('places/index', { places })
+
+})
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
