bd5073fb0e1670a83c094135ffc8523b9811cbc2
MyManny <gmail.com>
2023-09-07 18:15:25 -0700

    added seeders folder

--- views/error404.jsx
+++ views/error404.jsx
@@ -13,7 +13,7 @@ function error404 () {
               <div>
               Photo by <a href="https://unsplash.com/@matthewhenry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matthew Henry</a> on <a href="https://unsplash.com/photos/2Ts5HnA67k8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
-                </div>
+              </div>
           </main>
       </Def>
     )
